07b3d98bcdc98b17b49a8ff80f92cec0c06a224c
Satya Vardhan <satyacoc786@gmail.com>
2026-08-31 15:41:24 +0000

    add file

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
+  basePath: '/railvision',
   images: {
     unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
 
